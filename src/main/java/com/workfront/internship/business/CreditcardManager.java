a6da5fddca466597379acd8fc5527fa74f796d64
annaasmangulyan <annaasmangulyan@workfront.com>
2016-07-30 23:53:19 +0400

    CredidtCardNabaget writting budiness layer

--- src/main/java/com/workfront/internship/business/CreditcardManager.java
+++ src/main/java/com/workfront/internship/business/CreditcardManager.java
@@ -6,8 +6,8 @@ import com.workfront.internship.common.CreditCard;
  * Created by Workfront on 7/29/2016.
  */
 public interface CreditcardManager {
-    /*CreditCard getCreditCardByCardID(int cardid);
+    CreditCard getCreditCardByCardID(int cardid);
     int insertCreditCard(CreditCard creditCard);
     void updateCreditCard(CreditCard creditCard);
-    void deleteCreditCard(int id);*/
+    void deleteCreditCard(int id);
 }
--- src/main/java/com/workfront/internship/business/CreditcardManagerImpl.java
+++ src/main/java/com/workfront/internship/business/CreditcardManagerImpl.java
@@ -1,7 +1,39 @@
 package com.workfront.internship.business;
 
-/**
- * Created by Workfront on 7/29/2016.
- */
+import com.workfront.internship.common.CreditCard;
+import com.workfront.internship.dao.CreditCardDao;
+import com.workfront.internship.dao.CreditCardDaoImpl;
+import com.workfront.internship.dao.DataSource;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
 public class CreditcardManagerImpl implements CreditcardManager {
+    private DataSource dataSource;
+    private CreditCardDao creditCardDao;
+
+    public CreditcardManagerImpl(DataSource dataSource) throws IOException, SQLException {
+        this.dataSource = dataSource;
+        creditCardDao = new CreditCardDaoImpl(dataSource);
+    }
+
+    public CreditCard getCreditCardByCardID(int cardid){
+        if(cardid<=0)
+            throw new RuntimeException("invalid card id");
+        return creditCardDao.getCreditCardByCardID(cardid);
+
+    }
+    public int insertCreditCard(CreditCard creditCard){
+        if(!validateCard(creditCard))
+            throw new RuntimeException("invalid card");
+        int result = creditCardDao.
+
+    }
+    public void updateCreditCard(CreditCard creditCard){
+
+    }
+    public void deleteCreditCard(int id){
+
+    }
 }
