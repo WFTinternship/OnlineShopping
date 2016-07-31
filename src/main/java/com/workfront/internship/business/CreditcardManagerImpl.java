3f3fab9b28dc58d174dd824853828d722d18d379
annaasmangulyan <annaasmangulyan@workfront.com>
2016-07-31 11:04:31 +0400

    CreditCardManager finished

--- src/main/java/com/workfront/internship/business/CreditcardManagerImpl.java
+++ src/main/java/com/workfront/internship/business/CreditcardManagerImpl.java
@@ -27,13 +27,25 @@ public class CreditcardManagerImpl implements CreditcardManager {
     public int insertCreditCard(CreditCard creditCard){
         if(!validateCard(creditCard))
             throw new RuntimeException("invalid card");
-        int result = creditCardDao.
+        int result = creditCardDao.insertCreditCard(creditCard);
+        return result;
 
     }
     public void updateCreditCard(CreditCard creditCard){
+        if(!validateCard(creditCard))
+            throw new RuntimeException("invalid card");
+        creditCardDao.updateCreditCard(creditCard);
+
 
     }
-    public void deleteCreditCard(int id){
-
+    public void deleteCreditCard(int cardid){
+        if(cardid<=0)
+            throw new RuntimeException("invalid card id");
+        creditCardDao.deleteCreditCard(cardid);
+    }
+    private boolean validateCard(CreditCard creditCard){
+        if(creditCard !=null && creditCard.getBalance() >= 0.0 && creditCard.getBillingAddress() != null)
+            return true;
+        return false;
     }
 }
